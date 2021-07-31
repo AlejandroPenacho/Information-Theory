a21d49f9d31c3f8d55b7218e2f1d4f1130f38894
Alejandro <alejpr@kth.se>
2021-07-31 22:20:21 +0200

    SVG for compass added

--- src/svelte/SymbolEq.svelte
+++ src/svelte/SymbolEq.svelte
@@ -1,5 +1,27 @@
 <script lang="ts">
     let currentValue = 1;
-    let nSymbols = 2;
+
+    let coinSVGs = [
+        "/assets/svg/coins/darkCoin.svg",
+        "/assets/svg/coins/lightCoin.svg"
+    ]
+    let suitSVGs = [
+        "/assets/svg/suits/club.svg",
+        "/assets/svg/suits/diamond.svg",
+        "/assets/svg/suits/spade.svg",
+        "/assets/svg/suits/heart.svg"
+    ]
+
 </script>
 
+<div class="main">
+    <div>
+
+    </div>
+    <div>
+
+    </div>
+    <div>
+
+    </div>
+</div>
