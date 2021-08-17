00ba7aad42fc15e9a7b8efbc3dffe8ef715d0f9e
Alejandro <alejpr@kth.se>
2021-08-17 17:27:25 +0200

    Some explanations

--- src/svelte/coins/Explanation.svelte
+++ src/svelte/coins/Explanation.svelte
@@ -25,19 +25,25 @@
         <span class="p">63/64</span> * <span class="H">0023</span> + <span class="p">1/64</span> * 
         <span class="H">6</span> = <span class="H">0.116</span> bits.
     {:else if number===3}
-        The chance that the first 3 coins are heads is 1/16. This case is similar to the previous one: getting a
-        head is complicated (1 out of 16), but in that case, it provides a lot of information, 3 bits, as we know 
-        the first three outputs. Most of the times, however, this will be negative. In that case, we only know that 
-        at least one of the three first coins is head, which correspons to a value of information of <span class="H">0.093</span>
-        bits. At the end,
-        the entropy of this symbol is <span class="p">1/16</span>
+        The chance that the first 3 coins are heads is <span class="p">1/16</span>. This case is similar to the previous one: getting a
+        head is complicated (1 out of 8), but in that case, it provides a lot of information, <span class="H">3</span> bits, as we know 
+        the first three outputs. Most of the times, however, we will obtain a tail. In that case, we only know that 
+        at least one of the three first coins is head, which correspons to a value of information of <span class="H">0.193</span>
+        bits. At the end, the entropy of this symbol is <span class="p">1/8</span> * <span class="H">3</span> + 
+        <span class="p">7/8</span> * <span class="H">0.193</span> = 0.5435.
     {:else if number===4}
-        This case is similar to the previous one, but now, these 3 coins are not necessarily thre first ones. Then, there are 
-        more combinations of coins such that this symbols is positive.
+        This case is similar to the previous one, but now, these 3 coins are not necessarily the first ones. Then, there are 
+        more combinations of coins such that we get a head here. The chance of getting a head is 5 out of 16, that is, a 
+        probability of <span class="p">5/16</span>. Again, receiving a head gives more information than a tail.
     {:else if number===5}
-        Again, we have 0.5
+        In this case, the probability is the same as in the first case, and so, the entropy is the same, 
+        <span class="H">1</span> bit. In the first case, we could point out the value of the first coin from the 
+        result, and that is not the case here. How can these two provide the same information? The reason is that there 
+        are as many combinations of 6 coins in which 4 or more are positive, as there are combinations in which the first 
+        one is head.
     {:else if number===6}
-        For this case, where there are 3 possible asnwers, the entropy is obtained in the same way. 
+        In the message, unlike in the previous ones, there are 3 possible outputs. This does not change at all
+        the way entropy is analyzed. 
         <span class="p">x</span> * <span class="H">x</span> +
         <span class="p">x</span> * <span class="H">x</span> +
         <span class="p">x</span> * <span class="H">x</span> = <span class="H"> X</span>
